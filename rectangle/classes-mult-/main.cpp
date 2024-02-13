33d1f0c4f77ddc0ad9135938fc787a81bc6d4b55
Maria Haidukova <mashahaidukova@yandex.ru>
2024-02-13 21:55:15 +0300

    adding_A_folder_with_hpp

---
+++ rectangle/classes-mult-/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "rectangle.hpp"
+
+int main(){
+    Rectangle rectangle1;
+    std::cout << rectangle1.square() << std::endl;
+
+    Rectangle rectangle2(5.0, 3.9);
+    std::cout << rectangle2.square() << std::endl;
+    //ghj
+    //nnjkh
+}
